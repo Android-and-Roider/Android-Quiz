71374b17388b36bff61c61d7b635eb75657cf374
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-06-08 16:27:39 +0900

    Create SplashActivity.kt

---
+++ AndroidstudyProject/app/src/main/java/com/example/androidstudyproject/view/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidstudyproject.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidstudyproject.R
+import com.example.androidstudyproject.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.animationView.playAnimation()
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fadein, R.anim.fadeout)
+            finish()
+        }, DURATION)
+    }
+
+    companion object {
+        private const val DURATION: Long = 3000
+    }
+
+}
