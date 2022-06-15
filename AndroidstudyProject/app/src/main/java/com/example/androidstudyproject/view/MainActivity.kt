b8551b747dafed065854ab0ca54e5af465a16c0c
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-06-15 17:29:19 +0900

    feature 화면전환

--- AndroidstudyProject/app/src/main/java/com/example/androidstudyproject/view/MainActivity.kt
+++ AndroidstudyProject/app/src/main/java/com/example/androidstudyproject/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidstudyproject.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidstudyproject.R
 import com.example.androidstudyproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +12,15 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnMenuList.setOnClickListener {
-            val intent = Intent(this, ListActivity::class.java)
-            startActivity(intent)
+            val intent = Intent(this, ListActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+
+        binding.btnStartChoose.setOnClickListener {
+            val intent = Intent(this, RaffleActivity::class.java).apply {
+                startActivity(this)
+            }
         }
     }
 }
--- AndroidstudyProject/app/src/main/java/com/example/androidstudyproject/view/RaffleActivity.kt
+++ AndroidstudyProject/app/src/main/java/com/example/androidstudyproject/view/RaffleActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidstudyproject.view
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidstudyproject.R
 import com.example.androidstudyproject.databinding.ActivityRaffleBinding
 
 class RaffleActivity : AppCompatActivity() {
@@ -12,5 +14,20 @@ class RaffleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRaffleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        overridePendingTransition(R.anim.slide_enter,R.anim.none)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                overridePendingTransition(R.anim.none,R.anim.slide_down)
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
