790e55314c77ba2133303db7a7b6fe42ac8ef76b
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-06-29 22:38:31 +0900

    마무리
    Glide 라이브러리 이용 gif 이미지 로드

--- AndroidstudyProject/app/src/main/java/com/example/androidstudyproject/view/MainActivity.kt
+++ AndroidstudyProject/app/src/main/java/com/example/androidstudyproject/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidstudyproject.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.androidstudyproject.R
 import com.example.androidstudyproject.databinding.ActivityMainBinding
 
@@ -12,14 +13,17 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnMenuList.setOnClickListener {
-            val intent = Intent(this, ListActivity::class.java).apply {
+            Intent(this, ListActivity::class.java).apply {
                 startActivity(this)
             }
         }
         binding.btnStartChoose.setOnClickListener {
-            val intent = Intent(this, RaffleActivity::class.java).apply {
+            Intent(this, RaffleActivity::class.java).apply {
                 startActivity(this)
             }
         }
+
+        Glide.with(this).load(R.raw.thinkin).into(binding.imageView)
+
     }
 }
